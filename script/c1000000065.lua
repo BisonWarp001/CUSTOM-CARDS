d4793ab2462a9bd3f6d20d91614c4a025d4d689f
BisonWarp001 <b1s0n0p0l1smike@gmail.com>
2025-12-27 15:11:36 -0600

    Add files via upload

--- script/c1000000065.lua
+++ script/c1000000065.lua
@@ -2,14 +2,16 @@
 local s,id=GetID()
 
 function s.initial_effect(c)
-	-------------------------------------------------------
-	-- ACTIVATE (CANNOT BE NEGATED)
-	-------------------------------------------------------
+	-- Activate (once per Duel, cannot be negated)
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetCountLimit(1,id) -- once per Duel
-	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_CANNOT_NEGATE+EFFECT_FLAG_CANNOT_INACTIVATE)
+	e1:SetCountLimit(1,id)
+	e1:SetProperty(
+		EFFECT_FLAG_CANNOT_DISABLE+
+		EFFECT_FLAG_CANNOT_NEGATE+
+		EFFECT_FLAG_CANNOT_INACTIVATE
+	)
 	e1:SetCondition(s.actcon)
 	e1:SetCost(s.cost)
 	e1:SetTarget(s.target)
@@ -20,11 +22,11 @@ end
 s.listed_names={10000000}
 
 -------------------------------------------------------
--- CONDITION: CONTROL OBELISK (ORIGINAL DIVINE)
+-- CONDITION: Control Obelisk (original DIVINE)
 -------------------------------------------------------
 function s.obfilter(c)
 	return c:IsFaceup()
-		and c:IsCode(10000000) -- Obelisk the Tormentor
+		and c:IsCode(10000000)
 		and c:GetOriginalAttribute()==ATTRIBUTE_DIVINE
 end
 
@@ -33,7 +35,7 @@ function s.actcon(e,tp,eg,ep,ev,re,r,rp)
 end
 
 -------------------------------------------------------
--- COST: TRIBUTE 2 OTHER FACE-UP MONSTERS
+-- COST: Tribute 2 other face-up monsters
 -------------------------------------------------------
 function s.costfilter(c)
 	return c:IsFaceup()
@@ -50,7 +52,7 @@ function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 end
 
 -------------------------------------------------------
--- TARGET
+-- TARGET: Choose Obelisk
 -------------------------------------------------------
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then
@@ -70,23 +72,11 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 
 	---------------------------------------------------
-	-- EFFECTS OF OBELISK CANNOT BE NEGATED
-	---------------------------------------------------
-	local e0=Effect.CreateEffect(c)
-	e0:SetType(EFFECT_TYPE_SINGLE)
-	e0:SetCode(EFFECT_CANNOT_DISABLE)
-	e0:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-	e0:SetRange(LOCATION_MZONE)
-	e0:SetReset(RESET_EVENT|RESETS_STANDARD|RESET_PHASE|PHASE_END)
-	tc:RegisterEffect(e0)
-
-	---------------------------------------------------
-	-- INFINITE ATK (SIMULATED)
+	-- ATK = 9999999 (this turn)
 	---------------------------------------------------
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
 	e1:SetCode(EFFECT_SET_ATTACK_FINAL)
-	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
 	e1:SetValue(9999999)
 	e1:SetReset(RESET_EVENT|RESETS_STANDARD|RESET_PHASE|PHASE_END)
 	tc:RegisterEffect(e1)
@@ -97,12 +87,12 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_SINGLE)
 	e2:SetCode(EFFECT_PIERCE)
-	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
 	e2:SetReset(RESET_EVENT|RESETS_STANDARD|RESET_PHASE|PHASE_END)
 	tc:RegisterEffect(e2)
 
 	---------------------------------------------------
-	-- ACTIVATE LOCK (Ancient Gear–style)
+	-- ACTIVATION LOCK (from attack declaration
+	-- until end of Damage Step)
 	---------------------------------------------------
 	local e3=Effect.CreateEffect(c)
 	e3:SetType(EFFECT_TYPE_FIELD)
@@ -110,19 +100,23 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	e3:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
 	e3:SetRange(LOCATION_MZONE)
 	e3:SetTargetRange(0,1)
+	e3:SetCondition(s.lockcon)
 	e3:SetValue(s.aclimit)
-	e3:SetCondition(s.actcon1)
-	e3:SetReset(RESET_EVENT|RESETS_STANDARD|RESET_PHASE|PHASE_END)
+	e3:SetReset(RESET_EVENT|RESETS_STANDARD|RESET_PHASE|PHASE_DAMAGE)
 	tc:RegisterEffect(e3)
 end
 
 -------------------------------------------------------
--- ACTIVATE LOCK CONDITION
+-- LOCK CONDITION: ONLY WHEN OBELISK ATTACKS
 -------------------------------------------------------
-function s.aclimit(e,re,tp)
-	return re:IsHasType(EFFECT_TYPE_ACTIVATE) or re:IsMonsterEffect()
+function s.lockcon(e)
+	return Duel.GetAttacker()==e:GetHandler()
 end
 
-function s.actcon1(e)
-	return Duel.GetAttacker()==e:GetHandler()
+-------------------------------------------------------
+-- WHAT CANNOT BE ACTIVATED
+-------------------------------------------------------
+function s.aclimit(e,re,tp)
+	return re:IsActiveType(TYPE_SPELL+TYPE_TRAP)
+		or re:IsMonsterEffect()
 end
